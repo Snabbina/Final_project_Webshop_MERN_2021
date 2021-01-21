347ed32a106b558b24a1228535fae165a2fe7a47
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-01-21 15:32:36 +0100

    made route for the the products, set up dummy data to try it out, tested to send GET via postman, it worked there, getting database to show in MongoDB compas next.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,19 @@
-//dummy route
+
+const Product = require('../models/product')
+
+//Create new product => /api/v1/product/new
+exports.newProduct =  async (req, res, next) => {
+    const product = await Product.create(req.body)
+
+    res.status(201).json({
+        sucess: true,
+        product
+    })
+}
+
+
 exports.getProducts = (req, res, next) => {
+    
     res.status(200).json({
         sucess: true, 
         message: 'this route will show all the products in the databas'
