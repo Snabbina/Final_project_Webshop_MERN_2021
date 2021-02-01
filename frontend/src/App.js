d6834bef93587a35cde1fca30ebe96fdabc1f13f
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-02-01 13:55:31 +0100

    Made Footer, and some dummy css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
 import Header from './components/layouts/Header'
+import Footer from './components/layouts/Footer'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
