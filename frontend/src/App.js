aac8244bc8a27998dc4e9967c1fc1c0d2e6a0ea3
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-02-01 11:13:02 +0100

    frontend installed dependencies to React, cleand code

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <h1>MangoOils Webshop</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// ****************************************************************************
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//       <h1>MangoOils Webshop</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
