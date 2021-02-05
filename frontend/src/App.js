a5dd69bf96aea852a2fd2ecd9b354459600df852
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-02-05 08:35:15 +0100

    add cloudinary fro img handeling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 // import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
 import Home from "./components/Home";
