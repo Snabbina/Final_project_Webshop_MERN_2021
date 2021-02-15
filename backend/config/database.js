1979afe909a2b228b26107219e489cbe56971c1a
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-02-15 21:46:16 +0100

    updated gitignore

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,17 @@
-
-const mongoose = require('mongoose')
-
-
-
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_CLOUD_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
-    })   
-}
-  
-module.exports = connectDatabase
+  mongoose
+    .connect(process.env.DB_CLOUD_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
 
-
-
+module.exports = connectDatabase;
