c64b448a2aa54a8caaa307e31ca7cc8579093aad
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2021-02-23 12:50:36 +0100

    fixed app and server js bugs

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,15 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyparser = require("body-parser");
 const fileUpload = require("express-fileupload");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 const path = require("path");
 
 const errorMiddleware = require("./middlewares/errors");
 
 // Setting up config file
-if (process.env.NODE_ENV !== "PRODUCTION")
-  require("dotenv").config({ path: "backend/config/config.env" });
-// dotenv.config({ path: "backend/config/config.env" });
+// if (process.env.NODE_ENV !== "PRODUCTION")
+//   require("dotenv").config({ path: "backend/config/config.env" });
+dotenv.config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true }));
